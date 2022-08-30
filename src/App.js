44644a38440bccc7a9e62cbcbab99199b6c68f12
Andres Giacosa <giacosa.andres@gmail.com>
2022-08-30 11:01:54 -0300

    agrego navBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import bootstrap from 'bootstrap'
+import Navbar from './components/NavBar/NavBar';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,44 @@
+
+const NavBar = () => {
+   return (
+          <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid ">
+                    <a className="navbar-brand" href="#"> Hierba</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                    <div className="containter text-center">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">EXPERIENCIA</a>
+                                <ul className="dropdown-menu">
+                                    <li><a className="dropdown-item" href="#">SAHUMOS</a></li>
+                                    <li><a className="dropdown-item" href="#">VELAS</a></li>
+                                    <li><a className="dropdown-item" href="#">BRUMAS</a></li>
+                                    <li><a className="dropdown-item" href="#">PIEDRAS</a></li>
+                                    <li><a className="dropdown-item" href="#">BLENDS</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                TIENDA
+                                </a>
+                                <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">SAHUMOS</a></li>
+                                <li><a className="dropdown-item" href="#">VELAS</a></li>
+                                <li><a className="dropdown-item" href="#">PACKS</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">NOSOTROS</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                </div>
+        </nav>
+    );
+}
+
+export default NavBar;
