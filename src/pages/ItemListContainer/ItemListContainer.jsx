97157431dd2784ceadc64d4c92b5e6f8a6653a38
Andres Giacosa <giacosa.andres@gmail.com>
2022-09-26 18:40:09 -0300

    agrego el counter

--- src/pages/ItemListContainer/ItemListContainer.jsx
+++ src/pages/ItemListContainer/ItemListContainer.jsx
@@ -22,20 +22,22 @@ function cargoCategorias() {
     
     if (categoryId === undefined)
             {
-                getProducts().then(products => { setproductList(products);} )
+                
+                getProducts().then(products => { setproductList(products);} );
+                setLoader(false);
                 
             }else{
+                
             getProducts().then(prod => {
             setproductList(prod.filter(produ => produ.categoria === categoryId));
-            
+            setLoader(false);
             })
         }
     }
 
     /*Llamo la lista una vez montado el componente*/
     useEffect (() => {
-        setLoader(false);
-       cargoCategorias()
+       cargoCategorias();
     },[])
 
     
@@ -49,12 +51,13 @@ function cargoCategorias() {
         })
     }
 
-
-    if (loader) return (
+    if (loader) 
+    return (
         <div className="container text-center mt-5">
             <h3>Listando los articulos...</h3>
         </div>
     )
+
 
     return (
         <>
